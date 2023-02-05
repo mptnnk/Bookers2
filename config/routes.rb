fc6af24e2ff4330294fe7a276ebed12a28732a86
mptnnk <121846198+mptnnk@users.noreply.github.com>
2023-02-05 07:40:32 +0000

    bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  #get 'books/index'
+  #get 'books/show'
+  #get 'books/edit'
   #get 'users/show'
-  get 'users/:id'=>'users#show'
-  get 'users/edit'
-  get 'users/index'
-  get 'book/books'
-  get 'homes/top'
+  #get 'users/:id'=>'users#show'
+  #get 'users/edit'
+  #get 'users/index'
+  #get 'book/books'
+  #get 'homes/top'
   root to:'homes#top'
   get 'homes/about'=>'homes#about',as:"about"
   devise_for :users
+  
+  resources:books
+  resources:users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
